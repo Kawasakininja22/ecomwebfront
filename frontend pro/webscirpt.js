0c4cc40545ac49fa7a9270460c4df8f7cfcbf546
poorni_4622 <98221688+Kawasakininja22@users.noreply.github.com>
2023-04-16 19:57:14 +0530

    Add files via upload

---
+++ frontend pro/webscirpt.js
@@ -0,0 +1,45 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+ if(bar)
+{
+  bar.addEventListener('click', ()=>{
+    nav.classList.add('active');
+  })
+}
+ 
+if(close)
+{
+ close.addEventListener('click',()=>
+{
+  nav.classList.remove('active');
+})
+}
+
+ 
+ var Mainimg =  document.getElementById("Mainimg");
+    var Smallimg =  document.getElementsByClassName("small-img");
+   
+   Smallimg[0].onclick = function(){
+    Mainimg.src = Smallimg[0].src;
+   
+   }
+   
+   Smallimg[1].onclick = function(){
+    Mainimg.src = Smallimg[1].src;
+   
+   }
+   
+   Smallimg[2].onclick = function(){
+    Mainimg.src = Smallimg[2].src;
+   
+   }
+   
+   Smallimg[3].onclick = function(){
+    Mainimg.src = Smallimg[3].src;
+   
+   }
+
+
+  
